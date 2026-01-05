4971c678ec45afa922664fe650f7ee99fb95412e
Shelly <93165734+Sea-Shelly@users.noreply.github.com>
2026-01-04 23:05:50 -0500

    Basic Book
    added basic book before adding assets

---
+++ SubPages/ArtPages/Book.js
@@ -0,0 +1,63 @@
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+const book = document.querySelector("#book");
+
+
+const papers = document.querySelectorAll(".paper");
+const numPapers = papers.length;
+let curr = 1;
+const max = numPapers +1;
+
+var thing = document.querySelector(':root');
+
+papers.forEach((paper,index) =>{
+    paper.style.zIndex = numPapers - index;
+});
+
+
+// Event Listener
+prevBtn.addEventListener("click", goPrevPage);
+nextBtn.addEventListener("click", goNextPage);
+
+
+function openBook() {
+    book.style.transform = "translateX(35%)";
+    prevBtn.style.transform = "translateX(-30%)";
+    nextBtn.style.transform = "translateX(40%)";
+
+}
+
+function closeBook(isAtBeginning) {
+    if(isAtBeginning) {
+        book.style.transform = "translateX(5%)";
+    } else {
+        book.style.transform = "translateX(65%)";
+    }
+
+    prevBtn.style.transform = "translateX(0%)";
+    nextBtn.style.transform = "translateX(0%)";
+}
+
+function goNextPage() {
+    if(curr < max) {
+        const paper = papers[curr - 1];
+        paper.classList.add("flipped");
+        paper.style.zIndex = curr;
+
+        if (curr === 1) openBook();
+        if (curr === numPapers) closeBook(false);
+        curr++;
+    }
+}
+
+function goPrevPage() {
+    if(curr > 1) {
+        curr--;
+        const paper = papers[curr-1];
+        paper.classList.remove("flipped");
+        paper.style.zIndex = numPapers - curr + 1;
+
+        if(curr===1) closeBook(true);
+        if(curr === numPapers) openBook()
+    }
+}
